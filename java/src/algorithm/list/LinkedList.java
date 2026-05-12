221217de7dde720a24c5f7eb29766c228f21cac2
heesk0223@gmail.com <heesk0223@gmail.com>
2026-05-12 11:08:59 +0900

    Add LinkedList

--- java/src/algorithm/list/LinkedList.java
+++ java/src/algorithm/list/LinkedList.java
@@ -2,8 +2,140 @@ package algorithm.list;
 // javac algorithm/list/LinkedList.java
 // java algorithm/list/LinkedList
 
-public class LinkedList {
+import java.util.Comparator;
 
+
+public class LinkedList<E> {
+    class Node<E> {
+      private E data;
+      private Node<E> next;
+
+      Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+      }
+    }
+
+  private Node<E> head; // 머리 노드
+  private Node<E> crnt; // 현재 노드
+
+  public LinkedList() {
+    head = crnt = null;
+  }
+
+  // 노드를 검색 (선형 검색)
+  public E search(E obj, Comparator<? super E> c) { // Comparator: 객체의 순서를 결정하는 데 사용되는 인터페이스
+    Node<E> ptr = head; // 현재 스캔 중인 노드
+
+    while (ptr != null) {
+      if (c.compare(obj, ptr.data) == 0) {
+        crnt = ptr;
+        return ptr.data; // 검색 성공
+      }
+      ptr = ptr.next; // 다음 노드를 선택
+    }
+    return null; // 검색 실패
+  }
+
+  // 머리에 노드를 삽입
+  public void addFirst(E obj) {
+    Node<E> ptr = head; // 삽입 전의 머리 노드
+    head = crnt = new Node<>(obj, ptr);
+  }
+
+  // 꼬리에 노드를 삽입
+  public void addLast(E obj) {
+    if (head == null) { // 리스트가 비어 있으면
+      addFirst(obj); // 머리에 삽입
+    } else {
+      Node<E> ptr = head;
+      while (ptr.next != null) {
+        ptr = ptr.next; // 꼬리 노드를 찾음 
+      }
+      ptr.next = crnt = new Node<>(obj, null); // 새 노드를 꼬리에 삽입
+    }
+  }
+
+  // 머리 노드를 삭제
+  public void removeFirst() {
+    if (head != null) {
+      head = crnt = head.next; // 머리 노드를 다음 노드로 변경
+    }
+  }
+
+  // 꼬리 노드를 삭제
+  public void removeLast() {
+    if (head != null) {
+      if (head.next == null) { // 리스트에 노드가 하나만 있는 경우
+        removeFirst(); // 머리 노드를 삭제
+      } else {
+        Node<E> ptr = head;
+        Node<E> pre = head;
+        while (ptr.next != null) {
+          pre = ptr; // pre는 ptr의 바로 앞 노드
+          ptr = ptr.next; // ptr은 다음 노드로 이동
+        }
+        pre.next = null; // pre는 이제 꼬리 노드가 됨
+        crnt = pre; // 현재 노드를 pre로 설정
+      }
+    }
+  }
+
+  // 노드 p를 삭제
+  public void remove(Node<E> p) {
+    if (head != null) {
+      if (p == head) { // p가 머리 노드인 경우
+        removeFirst(); // 머리 노드를 삭제
+      } else {
+        Node<E> ptr = head;
+        while (ptr.next != p) {
+          ptr = ptr.next; // p의 바로 앞 노드를 찾음
+          if (ptr == null) return; // p가 리스트에 없는 경우
+        }
+        ptr.next = p.next; // p를 리스트에서 제거
+        crnt = ptr; // 현재 노드를 ptr로 설정
+      }
+    }
+  }
+
+  // 선택한 노드를 삭제
+  public void removeCurrentNode() {
+    remove(crnt);
+  }
+
+  // 모든 노드를 삭제
+  public void clear() {
+    while (head != null) { // 리스트가 비어 있을 때까지
+      removeFirst(); // 머리 노드를 삭제
+    }
+    crnt = null; // 현재 노드를 null로 설정
+  }
+
+  // 선택 노드를 하나 뒤쪽으로 진행
+  public boolean next() {
+    if (crnt == null || crnt.next == null) {
+      return false; // 진행할 수 없음
+    }
+    crnt = crnt.next; // 선택 노드를 뒤쪽으로 진행
+    return true;
+  }
+
+  // 선택 노드를 출력
+  public void printCurrentNode() {
+    if (crnt == null) {
+      System.out.println("선택한 노드가 없습니다.");
+    } else {
+      System.out.println(crnt.data);
+    }
+  }
+
+  // 모든 노드를 출력
+  public void dump() {
+    Node<E> ptr = head; // 스캔 중인 노드
     
-
+    while (ptr != null) {
+      System.out.println(ptr.data); // 노드의 데이터를 출력
+      ptr = ptr.next; // 다음 노드를 선택
+    }
+  }
 }
---
+++ java/src/algorithm/list/LinkedListTester.java
@@ -0,0 +1,164 @@
+package algorithm.list;
+// javac algorithm/list/LinkedList.java
+// java algorithm/list/LinkedList
+
+import java.util.Comparator;
+import java.util.Scanner;
+
+
+public class LinkedListTester {
+  static Scanner stdIn = new Scanner(System.in);
+
+  // 데이터 (회원 번호 + 이름)
+  static class Data {
+    static final int NO = 1; // 번호를 입력 받습니까?
+    static final int NAME = 2; // 이름을 입력 받습니까?
+
+    private Integer no; // 회원 번호
+    private String name; // 회원 이름
+
+    // 문자열을 반환하는 메서드
+    public String toString() {
+      return "(" + no + ") " + name;
+    }
+
+    // 데이터를 입력 받는 메서드
+    void scanData(String guide, int sw) {
+      System.out.println(guide + "할 데이터를 입력하세요.");
+
+      if ((sw & NO) == NO) { // 번호를 입력 받는 경우
+        System.out.print("번호: ");
+        no = stdIn.nextInt();
+      }
+      if ((sw & NAME) == NAME) { // 이름을 입력 받는 경우
+        System.out.print("이름: ");
+        name = stdIn.next();
+      }
+    }
+
+    public static final Comparator<Data> NO_ORDER = new NoOrderComparator();
+
+    private static class NoOrderComparator implements Comparator<Data> {
+      public int compare(Data d1, Data d2) {
+        return (d1.no > d2.no) ? 1 : (d1.no < d2.no) ? -1 : 0;
+      }
+    }
+
+    public static final Comparator<Data> NAME_ORDER = new NameOrderComparator();
+    
+    private static class NameOrderComparator implements Comparator<Data> {
+      public int compare(Data d1, Data d2) {
+        return d1.name.compareTo(d2.name);
+      }
+    }
+  }
+
+  enum Menu {
+    ADD_FIRST("머리에 노드 삽입"),
+    ADD_LAST("꼬리에 노드 삽입"),
+    RMV_FIRST("머리 노드 삭제"),
+    RMV_CRNT("선택 노드 삭제"),
+    CLEAR("모든 노드 삭제"),
+    SEARCH_NO("번호로 검색"),
+    SEARCH_NAME("이름으로 검색"),
+    NEXT("선택 노드를 뒤쪽으로 이동"),
+    PRINT_CRNT("선택 노드 출력"),
+    DUMP("모든 노드 출력"),
+    TERMINATE("종료");
+
+    private final String message; // 표시할 문자열
+
+    static Menu MenuAt(int idx) { // 순서가 idx인 열거를 반환
+      for (Menu m : Menu.values()) {
+        if (m.ordinal() == idx) {
+          return m;
+        }
+      }
+      return null;
+    }
+
+    Menu(String string) { // 생성자
+      message = string;
+    }
+
+    String getMessage() { // 표시할 문자열을 반환
+      return message;
+    }
+  }
+
+  // 메뉴 선택
+  static Menu selectMenu() {
+    int key;
+    do {
+      for (Menu m : Menu.values()) {
+        System.out.printf("(%d) %s  ", m.ordinal(), m.getMessage());
+        if ((m.ordinal() % 3) == 2 && m.ordinal() != Menu.TERMINATE.ordinal()) {
+          System.out.println();
+        }
+      }
+      System.out.print(" : ");
+      key = stdIn.nextInt();
+    } while (key < Menu.ADD_FIRST.ordinal() || key > Menu.TERMINATE.ordinal());
+    return Menu.MenuAt(key);
+  }
+
+  public static void main(String[] args) {
+    Menu menu; // 메뉴
+    Data data; // 추가용 데이터 참조
+    Data ptr; // 검색용 데이터 참조
+    Data temp = new Data(); // 입력 받는 데이터
+
+    LinkedList<Data> list = new LinkedList<Data>(); // 리스트를 생성
+
+    do { 
+        switch (menu = selectMenu()) {
+        case ADD_FIRST: // 머리에 노드 삽입
+          data = new Data();
+          data.scanData("머리에 삽입", Data.NO | Data.NAME);
+          list.addFirst(data);
+          break;
+        case ADD_LAST: // 꼬리에 노드 삽입
+          data = new Data();
+          data.scanData("꼬리에 삽입", Data.NO | Data.NAME);
+          list.addLast(data);
+          break;
+        case RMV_FIRST: // 머리 노드 삭제
+          list.removeFirst();
+          break;
+        case RMV_CRNT: // 선택 노드 삭제
+          list.removeCurrentNode();
+          break;
+        case CLEAR: // 모든 노드 삭제
+          list.clear();
+          break;
+        case SEARCH_NO: // 번호로 검색
+          temp.scanData("검색", Data.NO);
+          ptr = list.search(temp, Data.NO_ORDER);
+          if (ptr != null) {
+            System.out.println("검색 성공: " + ptr);
+          } else {
+            System.out.println("검색 실패");
+          }
+          break;
+        case SEARCH_NAME: // 이름으로 검색
+          temp.scanData("검색", Data.NAME);
+          ptr = list.search(temp, Data.NAME_ORDER);
+          if (ptr != null) {
+            System.out.println("검색 성공: " + ptr);
+          } else {
+            System.out.println("검색 실패");
+          }
+          break;
+        case NEXT: // 선택 노드를 뒤쪽으로 이동
+          list.next();
+          break;
+        case PRINT_CRNT: // 선택 노드 출력
+          list.printCurrentNode();
+          break;
+        case DUMP: // 모든 노드 출력
+          list.dump();
+          break;
+        }
+    } while (menu != Menu.TERMINATE);
+  }
+}
